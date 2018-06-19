d15ef19ab5ad2fcf804fb2d2f80d4832e5ca4176
Buddhi Adhikari <buddhiadikari@live.com>
2018-06-19 08:27:15 +0100

    adding json data for prediction is done

--- predictor.py
+++ predictor.py
@@ -5,11 +5,25 @@ import json
 
 # py list of lists with [height, weight, shoe size] data of each person (includes 11 people)
 # These data can be used for our gender prediction
-pplList = [[182, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40],
-    [159, 55, 37], [171, 75, 42], [181, 85, 43]]
-
+pplList=[]
 # Genders of the group of people
-pplGenderList = ['male', 'female','female', 'female', 'male', 'male', 'male', 'female', 'male', 'female', 'male',]
+pplGenderList=[]
+
+# populating the lists with data from 'data.json'
+with open("data.json") as json_file:
+    data = json.load(json_file)
+    for ppl in data['Data']:
+        pplList.append([ppl['height'],ppl['weight'],ppl['shoe size']])
+        pplGenderList.append(ppl['gender'])
+        
+    
+    # print(pplList)
+    # print(pplGenderList)
+
+# test values
+# pplList = [[182, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40],
+#     [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+# pplGenderList = ['male', 'female','female', 'female', 'male', 'male', 'male', 'female', 'male', 'female', 'male',]
 
 # Storing decision tree classifer in 'clf' var
 clf = tree.DecisionTreeClassifier()
